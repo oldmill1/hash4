efc58b23a1f19e1c0ea968697d675349a9f7f0e9
oldmill1 <ataxali@gmail.com>
2013-03-21 00:00:51 -0300

    Update index.php
    removed error_reporting

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 // ==========================================
 // Welcome to hash4 - a conventional flat-file blogging system
 // Licensed under The MIT License
